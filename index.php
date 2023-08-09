81d110bc3e7eb173fbc05edae6f7e73b08e54d6e
Luan Montanari <luanmontanari@hotmail.com>
2023-08-09 10:58:10 -0300

    bkp codigo inteiro

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -209,7 +210,40 @@ $app->post("/admin/forgot/reset",function(){
 	$page->setTpl("forgot-reset-success");
 
 });
-//echo date('H:i:s');
+
+$app->get("/admin/categories", function(){
+
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories",[
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	$page= new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
 
 $app->run();
 
